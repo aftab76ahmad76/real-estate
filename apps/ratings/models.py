20dd56353a21a2d93bacdf6d001d211b796df32b
Aftab Ahmad <aftab76ahmad76@gmail.com>
2022-10-21 19:45:46 +0500

    Added rating model & serializer

--- apps/ratings/models.py
+++ apps/ratings/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+from apps.common.models import TimeStampUUID
+from apps.profiles.models import Profile
+from project.settings.base import AUTH_USER_MODEL
+
+
+class Rating(TimeStampUUID):
+    class Range(models.IntegerChoices):
+        RATING_1 = 1, _("Poor")
+        RATING_2 = 2, _("Fair")
+        RATING_3 = 3, _("Good")
+        RATING_4 = 4, _("Very Good")
+        RATING_5 = 5, _("Excellent")
+
+    rater = models.ForeignKey(
+        AUTH_USER_MODEL,
+        verbose_name="USer providing the rating",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    agent = models.ForeignKey(
+        Profile,
+        verbose_name=_("Agent"),
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="agent_review",
+    )
+
+    rating = models.IntegerField(
+        verbose_name=_("Rating"),
+        default=0,
+        choices=Range.choices,
+        help_text="1=Poor, 2=Fair, 3=Good, 4=Very Good, 5=Excellent",
+    )
+
+    comment = models.TextField(verbose_name=_("Comment"))
+
+    class Meta:
+        unique_together = ["agent", "rater"]
+
+    def __str__(self):
+        return f"{self.agent} rated at {self.rating}"
